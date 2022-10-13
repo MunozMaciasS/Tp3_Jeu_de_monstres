7bf85c943dfb5d516897e7490491aff377ca9647
munozmaciass <munozmaciass@educ.college-montreal.qc.ca>
2022-10-13 10:36:32 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import arcade
+from random import randint
+
+while True:
+    answer = int(input('''   
+    Que voulez-vous faire ? 
+	1- Combattre cet adversaire
+	2- Contourner cet adversaire et aller ouvrir une autre porte
+	3- Afficher les règles du jeu
+	4- Quitter la partie
+    '''))
+    if answer = 1:
+
+    elif answer = 2:
+    elif answer = 3:
+    else:
+        print("D'accord\nMerci et aurevoir...")
+
+
+
+
